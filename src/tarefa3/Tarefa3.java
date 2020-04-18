5fd2ef6cf8bc8316843a0ba26cac4fc5405bd4ca
dcancelas <55793127+DanielCancelas@users.noreply.github.com>
2020-04-18 17:22:46 +0200

    Initial commit

---
+++ src/tarefa3/Tarefa3.java
@@ -0,0 +1,14 @@
+package tarefa3;
+
+/**
+ *
+ * @author dcancelas
+ */
+public class Tarefa3 {
+
+    public static void main(String[] args) {
+        Ventana vent = new Ventana();
+        vent.setVisible(true);
+    }
+    
+}
